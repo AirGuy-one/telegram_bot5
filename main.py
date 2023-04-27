c77c345ae7095d33db9691af573641a9ef90af08
AirGuy-one <airguy-one@yandex.ru>
2023-04-27 17:58:29 +0300

    Add the photo to answer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import logging
-import redis
 import requests
+import urllib.request
+
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from dotenv import load_dotenv
@@ -17,7 +17,9 @@ def start(update, context):
     products = requests.get(url, headers=headers).json()['data']
 
     keyboard = [
-        [InlineKeyboardButton(product['attributes']['name'], callback_data='1') for product in products]
+        [InlineKeyboardButton(product['attributes']['name'],
+                              callback_data=str(index)) for index, product in enumerate(products)
+         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -32,8 +34,41 @@ def echo(update, context):
     return ECHO
 
 
-# def handle_users_reply(update, context):
-#     if update.message.text == ''
+def handle_users_reply(update, context):
+    url_products = 'https://useast.api.elasticpath.com/pcm/products?include=main_image'
+    price_book_id = os.environ.get('PRICE_BOOK_ID')
+    url_prices = f'https://useast.api.elasticpath.com/pcm/pricebooks/{price_book_id}/prices'
+    headers = {
+        'Authorization': f'Bearer {os.environ.get("BEARER")}'
+    }
+    response = requests.get(url_products, headers=headers).json()
+
+    products = response['data']
+    prices = requests.get(url_prices, headers=headers).json()['data']
+    images = response['included']
+
+    product_index = int(update.callback_query.data)
+
+    product = products[product_index]
+    price = str(prices[product_index]['attributes']['currencies']['USD']['amount'])
+
+    image_href = images['main_images'][product_index]['link']['href']
+
+    name = product['attributes']['name']
+    description = product['attributes']['description']
+
+    message = f"{name}\n\n{price[:-2]}.{price[-2:]} per kg\n\n{description}"
+
+    urllib.request.urlretrieve(
+        image_href,
+        'image.png'
+    )
+
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open('image.png', 'rb'),
+        caption=message
+    )
 
 
 def main():
@@ -42,7 +77,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
-    # dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
